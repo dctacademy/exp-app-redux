a3ae47f592c5f9fa2005b4eaa0207adf7d36c9bc
Aniruddha Sg <aniruddhasg@Aniruddhas-MBP.Dlink>
2024-06-14 14:12:57 +0530

    initial commit

---
+++ src/reducers/categories-reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    data: []
+}
+
+const categoriesReducer  = (state = initialState, action) => {
+    switch(action.type) {
+
+        default: {
+            return {...state}
+        }
+    }
+}
+
+export default categoriesReducer
---
+++ src/reducers/expenses-reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    data: []
+}
+
+const expensesReducer = (state = initialState, action) => {
+    switch(action.type) {
+
+        default: {
+            return {...state}
+        }
+    }
+}
+
+export default expensesReducer
